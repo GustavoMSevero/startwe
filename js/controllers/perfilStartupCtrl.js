3dd7a6931c37ba5283ae10c5083b385ec0526b14
Gustavo M. Severo <gustavo.msevero@gmail.com>
2025-04-08 16:53:14 -0300

    Colocado campos obrigatórios no form e adicionado nome da responsável.

--- js/controllers/perfilStartupCtrl.js
+++ js/controllers/perfilStartupCtrl.js
@@ -1,25 +1,30 @@
-app.controller("perfilStartupCtrl", ['$scope', '$http', '$window', '$location', '$rootScope', function ($scope, $http, $window, $location, $rootScope) {
-
+app.controller("perfilStartupCtrl", [
+  "$scope",
+  "$http",
+  "$window",
+  "$location",
+  "$rootScope",
+  function ($scope, $http, $window, $location, $rootScope) {
     $scope.idusuario = localStorage.getItem("startwe_idusuario");
     $scope.usuario = localStorage.getItem("startwe_usuario");
-    console.log(`id usuario ${$scope.idusuario}, usuario ${$scope.usuario}`)
+    console.log(`id usuario ${$scope.idusuario}, usuario ${$scope.usuario}`);
 
-	if(location.hostname == 'localhost'){
-		var urlPrefix = 'http://localhost:8888/sistemas/Webapps/Projetos/startWe/api/registerStartup.php';
-	} else {
-		var urlPrefix = '';
-	}
+    if (location.hostname == "localhost") {
+      var urlPrefix =
+        "http://localhost:8888/web/startWe/api/registerStartup.php";
+    } else {
+      var urlPrefix = "api/registerStartup.php";
+    }
 
-	$scope.cadastrarPerfilStartup = function(perfilStartup){
-		perfilStartup.option = 'cadastro startup';
-		perfilStartup.idusuario = $scope.idusuario;
-		//console.log(perfilStartup)
-		$http.post(urlPrefix, perfilStartup).then(function(response){
-			//console.log(response.data)
-			$location.path('/feed')	
-		})
-
-	}
-
-	
-}]);
+    $scope.cadastrarPerfilStartup = function (perfilStartup) {
+      perfilStartup.option = "cadastro startup";
+      perfilStartup.nomeResponsavel = $scope.usuario;
+      perfilStartup.idusuario = $scope.idusuario;
+      //   console.log(perfilStartup);
+      $http.post(urlPrefix, perfilStartup).then(function (response) {
+        // console.log(response.data);
+        $location.path("/feed");
+      });
+    };
+  },
+]);
